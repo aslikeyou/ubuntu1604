514d63d434226d4e2fd64ecee3f75dc5803a0b14
Alexander Pekhota <aslikeyou@users.noreply.github.com>
2017-04-06 14:47:55 +0300

    Update 5install_lara_project.sh

--- 5install_lara_project.sh
+++ 5install_lara_project.sh
@@ -1,6 +1,6 @@
 
 sudo rm -rf /var/www/html
 read -p "Enter repository for clonning:" repo
+cat ~/.ssh/id_rsa.pub
 read -p "Copy id_rsa as deploy key into github and press Enter" yn
-echo ~/.ssh/id_rsa.pub
 sudo git clone $repo /var/www/html
