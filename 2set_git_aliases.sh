9c5168433fae7480581268209decb258c4bde0d1
Alexander Pekhota <aslikeyou@users.noreply.github.com>
2017-04-06 11:01:46 +0300

    Update 2set_git_aliases.sh

--- 2set_git_aliases.sh
+++ 2set_git_aliases.sh
@@ -7,3 +7,5 @@ git config --global alias.br branch
 git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
 git config --global alias.type 'cat-file -t'
 git config --global alias.dump 'cat-file -p'
+
+ssh-keygen -t rsa -b 4096 -C "alex@pdffiller.com"
