5ef9bfbd5faf2b215021a6069d46735fcde1d678
Alexander Pekhota <aslikeyou@users.noreply.github.com>
2017-04-06 14:14:58 +0300

    Update 4install_php.sh

--- 4install_php.sh
+++ 4install_php.sh
@@ -1,3 +1,4 @@
 
 sudo apt-get install -y php-fpm php-mysql
 sudo wget -q https://raw.githubusercontent.com/aslikeyou/ubuntu1604/master/php.ini -O /etc/php/7.0/fpm/php.ini
+sudo systemctl restart php7.0-fpm
