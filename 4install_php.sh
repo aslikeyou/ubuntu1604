65510e3a016d4709e6c35dccc14fa8f525fca439
Alexander Pekhota <aslikeyou@users.noreply.github.com>
2017-04-06 14:32:50 +0300

    Update 4install_php.sh

--- 4install_php.sh
+++ 4install_php.sh
@@ -1,7 +1,7 @@
 
 sudo apt-get install -y php-fpm php-mysql
 sudo rm /etc/php/7.0/fpm/php.ini
-sudo cat ./php.ini > /etc/php/7.0/fpm/php.ini
+sudo bash -c 'cat ./php.ini > /etc/php/7.0/fpm/php.ini'
 sudo systemctl restart php7.0-fpm
-sudo cat ./nginx_default.conf > /etc/nginx/sites-available/default
+sudo bash -c 'cat ./nginx_default.conf > /etc/nginx/sites-available/default'
 sudo systemctl reload nginx
