8f202a2c1baef663e0b14522c4c9e12cc64ff6d0
Alexander Pekhota <aslikeyou@users.noreply.github.com>
2017-04-07 17:39:10 +0300

    Update 5install_lara_project.sh

--- 5install_lara_project.sh
+++ 5install_lara_project.sh
@@ -5,3 +5,6 @@ cat ~/.ssh/id_rsa.pub
 read -p "Copy id_rsa as deploy key into github and press Enter" yn
 git clone $repo html
 sudo mv html /var/www
+
+sudo bash -c 'cat ./nginx_default.conf > /etc/nginx/sites-available/default'
+sudo systemctl reload nginx
