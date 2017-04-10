b3bd6571832c111013ca6616082d26ac94bfd236
Alexander Pekhota <aslikeyou@users.noreply.github.com>
2017-04-10 15:59:46 +0300

    Update 41_install_teamcity_agent.sh

--- 41_install_teamcity_agent.sh
+++ 41_install_teamcity_agent.sh
@@ -1,3 +1,10 @@
 
-
 sudo wget http://teamcity.pdffiller.com/update/buildAgent.zip -P /opt
+sudo unzip /opt/buildAgent.zip -d /opt/buildAgent
+SERVER_IP="$(/bin/hostname -i)"
+read -p "Enter the name for teamcity agent:" agentName
+
+cp ./buildAgent.properties.template ./buildAgent.properties
+sed -i "s/ownAddress=/${SERVER_IP}/g" ./buildAgent.properties
+sed -i "s/name=/${agentName}/g" ./buildAgent.properties
+mv ./buildAgent.properties /opt/buildAgent/conf/buildAgent.properties
