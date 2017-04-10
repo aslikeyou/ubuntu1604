df2d392b25f09cc0cd20806c11ff8bcccea6f5c5
Alexander Pekhota <aslikeyou@users.noreply.github.com>
2017-04-10 22:03:30 +0300

    Update 41install_teamcity_agent.sh

--- 41install_teamcity_agent.sh
+++ 41install_teamcity_agent.sh
@@ -1,6 +1,7 @@
 
 sudo wget http://teamcity.pdffiller.com/update/buildAgent.zip -P /opt
 sudo unzip /opt/buildAgent.zip -d /opt/buildAgent
+# http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-instance-addressing.html
 SERVER_IP="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
 read -p "Enter the name for teamcity agent:" agentName
 
