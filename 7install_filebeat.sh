7ff198677b1bc9478e902b2a0fb37e847955731c
Alexander Pekhota <aslikeyou@users.noreply.github.com>
2017-04-10 15:04:25 +0300

    Update 7install_filebeat.sh

--- 7install_filebeat.sh
+++ 7install_filebeat.sh
@@ -1,1 +1,8 @@
 
+#!/usr/bin/env bash
+
+sudo wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install -y apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+sudo apt-get update && sudo apt-get install filebeat
+sudo cp -rf filebeat /etc/
